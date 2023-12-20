c5863e8ca58ecff49259a4d77aba25c7e950fe70
SaTliX <122679887+SaTliX@users.noreply.github.com>
2023-12-20 17:12:17 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -75,9 +75,6 @@ class Carnet_adresses(ctk.CTk):
         self.frame_principal = ctk.CTkFrame(self, corner_radius=10)
         self.frame_principal.pack(pady=20, padx=20, fill="both", expand=True)
 
-        ctk.CTkLabel(self.frame_principal, text="Email :").grid(row=2, column=0, pady=10, padx=10)
-        ctk.CTkLabel(self.frame_principal, text="Téléphone :").grid(row=3, column=0, pady=10, padx=10)
-
         ctk.CTkLabel(self.frame_principal, text="Nom :").grid(row=0, column=0, pady=10, padx=10)
         self.nom_entry = ctk.CTkEntry(self.frame_principal)
         self.nom_entry.grid(row=0, column=1, pady=10, padx=10)
@@ -119,72 +116,46 @@ class Carnet_adresses(ctk.CTk):
         self.resultats_frame = ctk.CTkFrame(self.frame_principal, corner_radius=10)
         self.resultats_frame.grid(row=6, column=0, columnspan=4, pady=10, padx=10, sticky="nsew")
 
-        # Configuration du Treeview avec une liaison pour le tri
-        self.resultats_treeview = ttk.Treeview(self.resultats_frame, columns=("ID", "Nom", "Prénom", "Email", "Téléphone"), show='headings')
-        self.resultats_treeview.pack(fill="both", expand=True)
-
-        # Configurer les en-têtes de colonne
-        for col in self.resultats_treeview["columns"]:
-            self.resultats_treeview.heading(col, text=col)
-            self.resultats_treeview.column(col, width=100, anchor="center")
-
         self.carnet = CarnetAdresses()
-        self.contact_actuel = None  # Variable pour stocker les données du contact actuellement sélectionné
+        self.contact_actuel = None  
 
         self.afficher_tous_button = ctk.CTkButton(self.frame_principal, text="Afficher tous les contacts", fg_color="#0000ff", hover_color="#00008b", command=self.afficher_tous)
         self.afficher_tous_button.grid(row=7, column=0, columnspan=4, pady=10, padx=10)
 
-    def trier_treeview(self, tv, col, reverse):
-        # Récupérer les données de la colonne
-        liste = [(tv.set(k, col), k) for k in tv.get_children('')]
-        # Trier la liste
-        liste.sort(reverse=reverse)
+        style = ttk.Style()
 
-        # Réarranger les données dans le Treeview
-        for index, (val, k) in enumerate(liste):
-            tv.move(k, '', index)
-
-        # Inverser le tri pour la prochaine fois
-        tv.heading(col, command=lambda: self.trier_treeview(tv, col, not reverse))
-
-    def effacer_champs(self):
-        self.nom_entry.delete(0, tk.END)
-        self.prenom_entry.delete(0, tk.END)
-        self.email_entry.delete(0, tk.END)
-        self.telephone_entry.delete(0, tk.END)
-
-    def valider_numero_telephone(self, numero_telephone):
-        pattern = r"^\d{10}$"  # Format de numéro de téléphone à 10 chiffres (à adapter selon le format attendu)
-        return bool(re.match(pattern, numero_telephone))
-
-    def valider_email(self, email):
-        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        return bool(re.match(pattern, email))
     
-    def supprimer_contact(self):
-        id_contact = self.id_contact_entry.get()
-        if id_contact:
-            try:
-                id_contact = int(id_contact)
-                contact_existant = self.carnet.rechercher_contact_par_id(id_contact)
-                if contact_existant:
-                    confirmation = messagebox.askyesno("Confirmation de suppression", "Êtes-vous sûr de vouloir supprimer ce contact?")
-                    if confirmation:
-                        self.carnet.supprimer_contact(id_contact)
-                        messagebox.showinfo("Succès", "Contact supprimé avec succès")
-                        self.rechercher_contact()  # Actualiser la liste des contacts uniquement si le contact existe
-                else:
-                    messagebox.showwarning("Avertissement", "ID du contact introuvable")
-            except ValueError:
-                print("L'ID doit être un nombre entier.")
-        else:
-            messagebox.showwarning("Avertissement", "Veuillez entrer un ID du contact à supprimer.")
+        style.configure("Custom.Treeview.Heading", font=("Arial", 12, "bold"), foreground="blue")  
+
+        
+        self.resultats_treeview = ttk.Treeview(
+            self.resultats_frame,
+            columns=("ID", "Nom", "Prénom", "Email", "Téléphone"),
+            show='headings',
+            style="Custom.Treeview"  
+        )
+        self.resultats_treeview.pack(fill="both", expand=True)
+
+        
+        for col in self.resultats_treeview["columns"]:
+            self.resultats_treeview.heading(col, text=col, anchor="center", command=lambda c=col: self.trier_treeview(self.resultats_treeview, c, False))
+            self.resultats_treeview.column(col, width=100, anchor="center")
+
+        style.configure("Custom.Treeview", font=("arial", 12))  
+
+        
+        self.resultats_treeview.config(style="Custom.Treeview")
+        self.resultats_treeview["padding"] = 5
 
     def ajouter_contact(self):
         nom = self.nom_entry.get()
         prenom = self.prenom_entry.get()
         email = self.email_entry.get()
         telephone = self.telephone_entry.get()
+
+        if not all([nom, prenom, email, telephone]):
+            messagebox.showwarning("Avertissement", "Veuillez remplir tous les champs")
+            return
 
         if not self.valider_email(email):
             messagebox.showwarning("Avertissement", "Adresse e-mail non valide")
@@ -193,10 +164,22 @@ class Carnet_adresses(ctk.CTk):
         if not self.valider_numero_telephone(telephone):
             messagebox.showwarning("Avertissement", "Numéro de téléphone non valide")
             return
-        contact = Contact(self.nom_entry.get(), self.prenom_entry.get(), self.email_entry.get(), self.telephone_entry.get())
+
+        telephone_formate = ' '.join([telephone[i:i+2] for i in range(0, len(telephone), 2)])
+
+        contact = Contact(nom, prenom, email, telephone_formate)
         self.carnet.ajouter_contact(contact)
         messagebox.showinfo("Succès", "Contact ajouté avec succès")
-        self.rechercher_contact()  # Actualiser la liste des contacts
+        self.rechercher_contact()  
+
+    def valider_numero_telephone(self, numero_telephone):
+        pattern = r"^\d{10}$"  
+        return bool(re.match(pattern, numero_telephone))
+
+    def valider_email(self, email):
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return bool(re.match(pattern, email))
+    
 
     def rechercher_contact(self):
         id_contact = self.id_contact_entry.get()
@@ -206,12 +189,12 @@ class Carnet_adresses(ctk.CTk):
                 contact = self.carnet.rechercher_contact_par_id(id_contact)
                 self.resultats_treeview.delete(*self.resultats_treeview.get_children())  # Effacez les anciens résultats
                 if contact:
-                    # Affichez uniquement le contact correspondant dans le Treeview
                     self.resultats_treeview.insert("", tk.END, values=contact[0])
                 else:
                     messagebox.showwarning("Avertissement", "ID du contact introuvable")
-            except ValueError:
-                print("L'ID doit être un nombre entier.")
+            except:
+                messagebox.showwarning("Avertissement","L'ID doit être un nombre entier.")
+
 
     def rechercher_perso(self):
         nom = self.nom_entry.get()
@@ -219,14 +202,11 @@ class Carnet_adresses(ctk.CTk):
         telephone = self.telephone_entry.get()
         email = self.email_entry.get()
         
-        # Obtenez les résultats de la recherche en fonction des critères
         contacts = self.carnet.rechercher_perso(nom, prenom, telephone, email)
         
-        # Effacez toutes les lignes existantes du Treeview
         for row in self.resultats_treeview.get_children():
             self.resultats_treeview.delete(row)
 
-        # Insérez les résultats de la recherche dans le Treeview
         for contact in contacts:
             self.resultats_treeview.insert("", tk.END, values=contact)
 
@@ -250,7 +230,6 @@ class Carnet_adresses(ctk.CTk):
 
                 if contact_existant:
                     if self.contact_actuel is None:
-                        # Si c'est la première pression sur "Modifier", conservez les données du contact
                         self.contact_actuel = {
                             'id': id_contact,
                             'nom': contact_existant[0][1],
@@ -259,7 +238,6 @@ class Carnet_adresses(ctk.CTk):
                             'telephone': contact_existant[0][4]
                         }
 
-                        # Remplissez les champs avec les données du contact actuel
                         self.nom_entry.delete(0, tk.END)
                         self.nom_entry.insert(0, self.contact_actuel['nom'])
                         self.prenom_entry.delete(0, tk.END)
@@ -269,7 +247,6 @@ class Carnet_adresses(ctk.CTk):
                         self.telephone_entry.delete(0, tk.END)
                         self.telephone_entry.insert(0, self.contact_actuel['telephone'])
                     else:
-                        # Si c'est la deuxième pression sur "Modifier", mettez à jour les données du contact
                         self.carnet.modifier_contact(
                             self.contact_actuel['id'],
                             nouveau_nom,
@@ -278,12 +255,51 @@ class Carnet_adresses(ctk.CTk):
                             nouveau_telephone
                         )
                         messagebox.showinfo("Succès", "Contact modifié avec succès")
-                        self.contact_actuel = None  # Réinitialisez les données du contact actuel
-                        self.rechercher_contact()  # Actualiser la liste des contacts
+                        self.contact_actuel = None  
+                        self.rechercher_contact() 
+                else:
+                    messagebox.showwarning("Avertissement", "ID du contact introuvable")
+            except:
+               messagebox.showwarning("Avertissement","L'ID doit être un nombre entier.")
+
+
+    def supprimer_contact(self):
+        id_contact = self.id_contact_entry.get()
+        if id_contact:
+            try:
+                id_contact = int(id_contact)
+                contact_existant = self.carnet.rechercher_contact_par_id(id_contact)
+                if contact_existant:
+                    confirmation = messagebox.askyesno("Confirmation de suppression", "Êtes-vous sûr de vouloir supprimer ce contact?")
+                    if confirmation:
+                        self.carnet.supprimer_contact(id_contact)
+                        messagebox.showinfo("Succès", "Contact supprimé avec succès")
                 else:
                     messagebox.showwarning("Avertissement", "ID du contact introuvable")
             except ValueError:
-                print("L'ID doit être un nombre entier.")
+                messagebox.showwarning("Avertissement", "L'ID doit être un nombre entier.")
+        else:
+            messagebox.showwarning("Avertissement", "Veuillez entrer un ID du contact à supprimer.")
+
+
+    def trier_treeview(self, tv, col, reverse):
+        
+        liste = [(tv.set(k, col), k) for k in tv.get_children('')] # données colonne 
+       
+        liste.sort(reverse=reverse)
+
+        
+        for index, (val, k) in enumerate(liste):
+            tv.move(k, '', index)
+
+        
+        tv.heading(col, command=lambda: self.trier_treeview(tv, col, not reverse))
+
+    def effacer_champs(self):
+        self.nom_entry.delete(0, tk.END)
+        self.prenom_entry.delete(0, tk.END)
+        self.email_entry.delete(0, tk.END)
+        self.telephone_entry.delete(0, tk.END)
 
 if __name__ == "__main__":
     app = Carnet_adresses()
